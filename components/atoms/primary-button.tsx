d908bc24fc3348421a305e6c48649ee0ac27831d
fernando12mais <fernandogabriel12mais@gmail.com>
2023-09-24 21:54:15 -0300

    Adds hover state to button

--- components/atoms/primary-button.tsx
+++ components/atoms/primary-button.tsx
@@ -6,7 +6,7 @@ export default function PrimaryButton(props: ComponentProps<'button'>) {
     <button
       {...props}
       className={twMerge(
-        'flex items-center justify-center gap-4 rounded-[50px] bg-action p-4 text-sm font-bold uppercase text-background lg:gap-5 lg:text-xl',
+        'flex items-center justify-center gap-4 rounded-[50px] bg-action p-4 text-sm font-bold uppercase text-background hover:bg-emerald-600 active:bg-emerald-800 lg:gap-5 lg:text-xl',
         props.className
       )}
     >
